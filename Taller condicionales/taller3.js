453555fe80689bdc5bf07faa477de8ad0669aeed
Sebastian Serna Arango <161923093+Darkot1@users.noreply.github.com>
2024-04-25 21:30:48 -0500

    Add files via upload

--- Taller condicionales/taller3.js
+++ Taller condicionales/taller3.js
@@ -42,32 +42,32 @@ if (numberdiv %3 == 0) {
 
 let = number100 = 103;
 if (number100 > 100) {
-    console.log(`El numero: ${number100} es mayor a 100 :D`)
+    console.log(`El numero: ${number100} es mayor a 100 :D`);
 }else console.log("El numero no es mayor a 100 :c")
 
 //Punto 8
 let numberMe = -46;
 if (numberMe >= -50 && numberMe <= 0){
-    console.log(`El numero ${numberMe} es menor de -50`)
+    console.log(`El numero ${numberMe} es menor de -50`);
 }else console.log("El numero no es menor que -50");
 
 //Punto 9
 let numberRange = 32;
 if (numberRange >= 20 && numberRange <= 50){
-    console.log(`El numero ${numberRange} esta en el rango`)
+    console.log(`El numero ${numberRange} esta en el rango`);
 }else console.log(`El numero ${numberRange} no esta en el rango`);
 
 //Punto 10
 
 let numberZero = 0;
 if (numberZero == 0){
-    console.log(`El numero es ${numberZero}`)
+    console.log(`El numero es ${numberZero}`);
 }else console.log(`El numero ${numberZero} no es cero`);
 
 //Punto 11
 let numberTo =  5;
 if (numberTo >= -10 && numberTo <= 10){
-    console.log(`El numero ${numberTo} es menor de -10 y mayor que 10`)
+    console.log(`El numero ${numberTo} es menor de -10 y mayor que 10`);
 }else console.log("El numero no esta en rango");
 
 //Punto 12
@@ -88,9 +88,43 @@ if (persona >= 18){
 //N/N
 
 //Punto 15
-let cuadrado = 5;
+let cuadrado = 9;
 if (Math.sqrt(cuadrado) % 1 === 0){
-    console.log(`El numero ${cuadrado} es un cuadrado perfecto`)
-}else console.log(`El numero ${cuadrado} NO es cuadrado perfecto`)
+    console.log(`El numero ${cuadrado} es un cuadrado perfecto`);
+}else console.log(`El numero ${cuadrado} NO es cuadrado perfecto`);
 
 //Punto 16
+let fibo = 6;
+
+
+
+//Punto 17
+let potencia = 16;
+if(potencia > 0 &&(potencia & potencia -1)===0){
+    console.log(`El numero ${potencia} es potencia de 2`);
+} else console.log(`El numero ${potencia} no es potencia de 2`);
+
+//Punto 18
+
+let palindromo = 5 
+
+//Punto 19
+let frase = 'hola JavaScript';
+console.log(frase.includes('JavaScript') ? 'SI tiene la frase JavaScript' : 'NO tiene la palabra JavaScript');
+
+
+//Punto 20
+let cadena = 'isla puntacana';
+let contarCadena = cadena.length;
+console.log(contarCadena >= 10 ? `La cadena ${cadena} tiene ${contarCadena} caracteres SI CUMPLE` : `la cadena ${cadena} tiene ${contarCadena} NO CUMPLE`);
+
+//Punto 21
+let minuscula = 'HOLA'
+console.log(minuscula.toLocaleLowerCase() === minuscula ? `La frase ${minuscula} ESTA en minuscula` : `la frase ${minuscula} NO esta en minuscula`);
+
+//Punto 22
+let frase2 = 'hola 20';
+console.log(frase2.match(/\d/)? `la frase ${frase2} tiene un numero `: `la frase ${frase2} NO tiene un numero`);
+
+//Punto 23
+
