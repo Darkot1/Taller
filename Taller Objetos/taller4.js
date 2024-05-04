b72edb496b4aa1fb2b33298b48f225183a22a064
Darkot1 <sebarango136@gmail.com>
2024-05-04 14:21:54 -0500

    update

--- Taller Objetos/taller4.js
+++ Taller Objetos/taller4.js
@@ -68,8 +68,6 @@ const rectangulo = {
     ancho: 10,
     largo: 5,
 }
-
-
 //Punto 14
 
 let areaRectangulo = 2 * (rectangulo.ancho + rectangulo.largo);
@@ -79,6 +77,128 @@ console.log(areaRectangulo);
 const formas = {...circulo,...rectangulo};
 console.log(formas);
 
+//PUNTO 16
+const computador = {
+    marca:"Asus",
+    modelo:"2023",
+    precio: 2500000
+};
 
+//PUNTO 17
+console.log(`el precio de la computadora es ${computador.precio}`);
 
+//PUNTO 18
+computador["tarjetaGrafica"]="No";
 
+//PUNTO 19
+const mascota = {
+    nombre:"safiro",
+    especie:"gato",
+    edad:5
+};
+
+//PUNTO 20
+console.log(mascota.especie.toUpperCase());
+
+//PUNTO 21 y 22
+const fruta ={
+    nombre:"Manzana",
+    color:"Roja",
+    estado:"madura",
+    verificarMadurez: function() {
+        if(this.estado === "madura"){
+            console.log(`la ${this.nombre} esta madura`)
+        }else console.log(`la ${this.nombre} no esta madura`)
+    }
+};
+
+fruta.verificarMadurez();
+
+//PUNTO 23, 24 y 25
+const estudiante = {
+    nombre:"Pedro",
+    edad: 17,
+    calificaciones:[3.4, 3.2, 4.1, 2.5, 3.5],
+    promedio: function(){
+        let total=0;
+        for (const nota of this.calificaciones) {
+            total+= nota;
+        }
+        const promedio = total /this.calificaciones.length;
+        console.log(`El promedio del estudiante ${this.nombre} es: ${promedio}`);
+        console.log(promedio >= 3.0 ? `El estudiante aprobo` : `El estudiante reprobo`)
+    },
+};
+
+estudiante.promedio();  
+
+//PUNTO 26, 27 y 28
+const bolsa = {
+    tamaño: 10,
+    color: "negra",
+    mostrarTamaño: function() {
+        console.log(`La bolsa es de tamaño ${this.tamaño}.`);
+},
+cambiarColor: function(nuevoColor) {
+    this.color = nuevoColor;
+    console.log(`La bolsa cambio de color a ${this.color}.`);
+}
+};
+
+bolsa.mostrarTamaño();
+bolsa.cambiarColor("verde");
+
+//Punto 29, 30 y 31
+const telefono = {
+    marca:"samsung",
+    modelo:"S24",
+    sistemaOperativo: "Android",
+    RAM: 8,
+    mostrarSistema: function(){
+        console.log(`El sistema operativo del telefono ${this.marca} ${this.modelo} es: ${this.sistemaOperativo} y la RAM es de ${this.RAM}Gb`)
+    }
+
+};
+telefono.mostrarSistema();
+
+//PUNTO 32 Y 34
+const animal = {
+    tipo:'Vaca',
+    sonido:'Muuu',
+    mostrarSonido: function(){
+        console.log(`El animal es una ${this.tipo} y su sonido es ${this.sonido}`)
+    }
+};
+animal.mostrarSonido();
+
+//PUNTO 33
+console.log(`el sonido del animal es: ${animal.sonido}`);
+
+//PUNTO 35
+const vuelo ={
+    aerolinea: "AirLatam",
+    numeroVuelo: 25,
+    horaSalida: "3:15 PM"
+};
+
+//PUNTO 36
+console.log(`Su aerolinea es: ${vuelo.aerolinea} y el numero de vuelo es el: ${vuelo.numeroVuelo}`);
+
+//PUNTO 37
+vuelo.destino = "Madrid";
+console.log(vuelo);
+
+//PUNTO 38
+const jugador = {
+    nombre:"maikol",
+    equipo:"Aguilas",
+    posicion:"Central",
+    mostrarPosicion: function(){
+        console.log(`La posicion del jugador ${this.nombre} es: ${this.posicion}`);
+    }
+};
+//PUNTO 39
+console.log(`El jugador ${jugador.nombre} esta en el equipo ${jugador.equipo}.`);
+
+//PUNTO 40
+jugador.mostrarPosicion();
